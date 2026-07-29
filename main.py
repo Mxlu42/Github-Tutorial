542f5b41f6ee9c9136e48d19c82d18b724328933
Mxlu42 <166992193+Mxlu42@users.noreply.github.com>
2026-07-29 21:32:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ class world:
     def greeting_world(self):
         return 'Hello World'
 
+    def hallo_welt(self):
+        print('Hallo Welt')
+
+    def ist_schaltjahr(self, jahr):
+        return jahr % 4 == 0 and (jahr % 100 != 0 or jahr % 400 == 0)
+
 
 if __name__ == '__main__':
     b = 'hello'
